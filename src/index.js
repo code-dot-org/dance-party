3a17fd32ab090ea6fbd0beb89b05773901f56b8c
Josh Lory <josh.lory@code.org>
2018-10-23 10:45:06 -0700

    Extract out `loadP5` helper

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import p5 from 'p5';
-window.p5 = p5; // Needed for p5.play init.
-require('@code-dot-org/p5.play/lib/p5.play');
+import loadP5 from './loadP5';
 import DanceParty from './p5.dance';
 import macklemore90 from '../metadata/macklemore90.json';
 import hammer from '../metadata/hammer.json';
 import peas from '../metadata/peas.json';
 
-new window.p5(p5Inst => {
+loadP5().then(p5Inst => {
   const nativeAPI = window.nativeAPI = new DanceParty(p5Inst, {
     getSelectedSong: () => "hammer",
     onPuzzleComplete: () => {},
