716e3dcce4c82abf46396d814ed7f0e26864b46e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-10-30 13:34:16 -0700

    expose constants and p5 initialization to including context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const DanceParty = require('./p5.dance');
+const constants = require('./constants');
+const loadP5 = require('./loadP5');
 
 module.exports = {
   DanceParty,
+  constants,
+  loadP5
 };
