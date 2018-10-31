8bffcd367f4042327f6e986f763066bd7141e2ab
Josh Caldwell <josh@code.org>
2018-10-31 08:59:23 -0700

    Add text background effect

--- src/Effects.js
+++ src/Effects.js
@@ -161,6 +161,42 @@ module.exports = class Effects {
       }
     };
 
+    this.text = {
+      texts: [],
+      maxTexts: 100,
+      update: function (text, hue, size) {
+        this.texts.push({
+          x: randomNumber(25, 375),
+          y: randomNumber(25, 375),
+          text: text,
+          color: colorFromHue(hue),
+          size: size
+        });
+        if (this.texts.length > this.maxTexts) {
+          this.texts.shift();
+        }
+      },
+      draw: function ({isPeak, centroid, artist, title}) {
+        if (isPeak) {
+          let text;
+          if (randomNumber(0, 1) === 0) {
+            text = artist;
+          } else {
+            text = title;
+          }
+          this.update(text, centroid, randomNumber(14, 48));
+        }
+        p5.push();
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        this.texts.forEach(function (t) {
+          p5.textSize(t.size);
+          p5.fill(t.color);
+          p5.text(t.text, t.x, t.y);
+        });
+        p5.pop();
+      }
+    };
+
     this.raining_tacos = {
       tacos: [],
       size: 50,
