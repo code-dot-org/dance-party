04af5ee32940c4fbe816425da9e87a7efc65e9de
Josh Caldwell <josh@code.org>
2018-10-31 21:16:40 -0700

    Add color lights foreground effect

--- src/Effects.js
+++ src/Effects.js
@@ -369,5 +369,45 @@ module.exports = class Effects {
         p5.pop();
       }
     };
+    this.color_lights = {
+      lights: [],
+      newLight: function (x, arc, offset) {
+        return {
+          x: x,
+          arc: arc,
+          offset: offset,
+          shift: randomNumber(0, 100),
+          color: randomColor(100, 50, 0.25),
+        };
+      },
+      init: function () {
+        this.lights.push(this.newLight(75, 25, -10));
+        this.lights.push(this.newLight(100, 15, -15));
+        this.lights.push(this.newLight(300, 15, 15));
+        this.lights.push(this.newLight(325, 25, 10));
+      },
+      update: function () {
+        this.lights.forEach(function (light) {
+          light.color = randomColor(100, 50, 0.25);
+        });
+      },
+      draw: function ({isPeak, bpm, centroid}) {
+        if (this.lights.length<1) {
+          this.init();
+        }
+        if (isPeak) {
+          this.update();
+        }
+        p5.noStroke();
+        this.lights.forEach(function (light) {
+          p5.push();
+          p5.fill(light.color);
+          p5.translate(light.x, -50);
+          p5.rotate((Math.sin((p5.frameCount / 100) + light.shift + centroid / 1000) * light.arc) + light.offset);
+          p5.triangle(0, 0, -75, 600, 75, 600);
+          p5.pop();
+        })
+      }
+    };
   }
 };
