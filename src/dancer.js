7ed8a186b16a8e26edb6bb8c2bc3e06b4dbeeb0b
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-11-02 14:55:57 -0700

    On-demand rasterizer

--- src/dancer.js
+++ src/dancer.js
@@ -1,44 +1,107 @@
+
 module.exports = class Dancer {
+  constructor(type = 'cat') {
+    this.clapHigh = new Move(type, 0);
+  }
+
+  drawPose(...args) {
+    this.clapHigh.drawPose(...args);
+  }
+};
+
+class Rasterizer {
   constructor() {
+    this.cache = {/*
+      dancerName_frameNum: ImageData
+    */};
+    this.queue = [/*
+      frameKey: `character_move_framenum`
+    */];
+
     this.defaultWidth = 300;
     this.defaultHeight = 300;
+  }
 
-    this.frames = [];
-    let img = new Image();
-    let reference = document.createElement('canvas');
-    let referenceCtx = reference.getContext('2d');
-    img.onload = () => {
-      reference.width = reference.height = 7200;
-      referenceCtx.drawImage(img, 0, 0, 480, 480, 0, 0, 7200, 7200);
-        let canvas = document.createElement('canvas');
-        let ctx = canvas.getContext('2d');
-        for (let i = 0; i < 12; i++) {
-          for (let j = 0; j < 24; j++) {
-            canvas.width = canvas.height = 300;
-            ctx.drawImage(reference, j * 300, i * 300, 300, 300, 0, 0, 300, 300);
-            // canvas.toBlob(blob => {
-            //   console.log("converted " + j);
-            //   this.frames[j] = blob;
-            // });
-            createImageBitmap(canvas).then(bm => this.frames[j] = bm);
-          }
-        }
-      img = null;
-      referenceCtx = null;
-      reference = null;
-      ctx = null;
-      canvas = null;
+  startQueue() {
+    // Setup memory
+    this.img = new Image();
+    this.reference = document.createElement('canvas');
+    this.referenceCtx = this.reference.getContext('2d');
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    setTimeout(this.processQueue.bind(this), 0);
+  }
+
+  processQueue() {
+    if (this.queue.length <= 0) {
+      // Cleanup memory
+      this.img = null;
+      this.referenceCtx = null;
+      this.reference = null;
+      this.ctx = null;
+      this.canvas = null;
+      return;
+    }
+
+    this.rasterizeItem(this.queue[0], () => {
+      this.queue.shift();
+      this.processQueue();
+    });
+  }
+
+  rasterizeItem(frameKey, callback) {
+    const [character, move] = frameKey.split('_');
+    this.img.onload = () => {
+      this.reference.width = 7200;
+      this.reference.height = 300;
+      this.referenceCtx.drawImage(this.img, 0, move * 20, 480, 20, 0, 0, 7200, 300);
+      for (let j = 0; j < 24; j++) {
+        this.canvas.width = this.canvas.height = 300;
+        this.ctx.drawImage(this.reference, j * 300, 0, 300, 300, 0, 0, 300, 300);
+        this.cache[this.frameKey(character, move, j)] = this.ctx.getImageData(0, 0, 300, 300);
+      }
+      callback();
     };
-    img.src = 'cat.min.svg';
+    this.img.src = 'cat.min.svg'; //`assets/${character}.svg`;
+  }
+
+  frameKey(character, move, frame) {
+    return `${character}_${move}_${frame}`;
+  }
+
+  getFrame(character, move, frame) {
+    const frameKey = this.frameKey(character, move, frame);
+    if (this.cache[frameKey]) {
+      return this.cache[frameKey];
+    }
+
+    if (!this.queue.some(key => key.startsWith(`${character}_${move}_`))) {
+      this.queue.push(frameKey);
+      this.startQueue();
+    }
+    return null;
+  }
+}
+
+const rasterizer = new Rasterizer();
+
+class Move {
+  constructor(character, move) {
+    this.character = character;
+    this.move = move;
   }
 
   drawPose(ctx, n, centerX, centerY, scaleX = 1, scaleY = 1, tint = null) {
-    ctx.drawImage(
-      this.frames[n],
+    const frame = rasterizer.getFrame(this.character, this.move, n);
+
+    if (!frame) {
+      return;
+    }
+
+    ctx.putImageData(
+      frame,
       centerX - this.defaultWidth / 2,
-      centerY - this.defaultHeight / 2,
-      this.defaultWidth * scaleX,
-      this.defaultHeight * scaleY
+      centerY - this.defaultHeight / 2
     );
   }
-};
+}
