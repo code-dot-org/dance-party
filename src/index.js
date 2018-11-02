34ded1d2a888bb8d0322a0c7857ead41a2e9cce1
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-11-02 15:02:31 -0700

    don't provide loadp5; it doesn't work in lambda

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const DanceParty = require('./p5.dance');
 const constants = require('./constants');
-const loadP5 = require('./loadP5');
 
 module.exports = {
   DanceParty,
   constants,
-  loadP5
 };
