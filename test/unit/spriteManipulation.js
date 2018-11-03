2490c4bafaffe078db444f1a81d6f974ec5700c8
Chris Pirich <chris@code.org>
2018-11-03 16:37:44 -0700

    add several new test cases

--- test/unit/spriteManipulation.js
+++ test/unit/spriteManipulation.js
@@ -99,6 +99,158 @@ test('Sprite dance changes to a new dance for random', async t => {
   nativeAPI.reset();
 });
 
+test('Sprite dance changes with next/prev/rand avoids rest dance', async t => {
+
+  const subTest = async testCode => {
+    const nativeAPI = await helpers.createDanceAPI();
+    nativeAPI.play({
+      bpm: 120,
+    });
+  
+    // Mock 3 cat animation poses
+    const moveCount = 3;
+    for(let i = 0; i < moveCount; i++) {
+      nativeAPI.setAnimationSpriteSheet("CAT", i, {}, () => {});
+      nativeAPI.world.MOVE_NAMES.push({
+        name: `move${i}`,
+        rest: i == 0,
+      })
+    }
+    nativeAPI.world.fullLengthMoveCount = moveCount;
+    nativeAPI.world.restMoveCount = 1;
+  
+    const sprite = nativeAPI.makeNewDanceSprite("CAT", null, {x: 200, y: 200});  
+
+    testCode({ nativeAPI, sprite });
+
+    nativeAPI.reset();
+  }
+
+  // Verify Next behavior:
+  await subTest(({ nativeAPI, sprite }) => {
+    // Initial value
+    t.equal(sprite.current_move, 0);
+    nativeAPI.changeMoveLR(sprite, 'next', 1);
+    // Index 1
+    t.equal(sprite.current_move, 1);
+    nativeAPI.changeMoveLR(sprite, 'next', 1);
+    // Index 2
+    t.equal(sprite.current_move, 2);
+    nativeAPI.changeMoveLR(sprite, 'next', 1);
+    // Loops back to index 1 (skipping 0, which is the rest dance)
+    t.equal(sprite.current_move, 1);
+  });
+
+  // Verify Prev behavior:
+  await subTest(({ nativeAPI, sprite }) => {
+    // Initial value
+    t.equal(sprite.current_move, 0);
+    nativeAPI.changeMoveLR(sprite, 'prev', 1);
+    // Index 2
+    t.equal(sprite.current_move, 2);
+    nativeAPI.changeMoveLR(sprite, 'prev', 1);
+    // Index 1
+    t.equal(sprite.current_move, 1);
+    nativeAPI.changeMoveLR(sprite, 'prev', 1);
+    // Loops back to index 2 (skipping 0, which is the rest dance)
+    t.equal(sprite.current_move, 2);
+  });
+
+  // Verify Rand behavior:
+  await subTest(({ nativeAPI, sprite }) => {
+    // Initial value
+    t.equal(sprite.current_move, 0);
+    nativeAPI.changeMoveLR(sprite, 1, 1);
+    // Index 1
+    t.equal(sprite.current_move, 1);
+    nativeAPI.changeMoveLR(sprite, 'rand', 1);
+    // Must be Index 2 (must be different and not 0, which is the rest dance)
+    t.equal(sprite.current_move, 2);
+    nativeAPI.changeMoveLR(sprite, 'rand', 1);
+    // Must be Index 1 (must be different and not 0, which is the rest dance)
+    t.equal(sprite.current_move, 1);
+  });
+
+  t.end();
+});
+
+test('Sprite dance changes will throw with invalid parameters', async t => {
+
+  const subTest = async ({ moveCount = 3, testCode }) => {
+    const nativeAPI = await helpers.createDanceAPI();
+    nativeAPI.play({
+      bpm: 120,
+    });
+  
+    // Mock cat animation poses
+    for(let i = 0; i < moveCount; i++) {
+      nativeAPI.setAnimationSpriteSheet("CAT", i, {}, () => {});
+      nativeAPI.world.MOVE_NAMES.push({
+        name: `move${i}`,
+        rest: i == 0,
+      })
+    }
+    nativeAPI.world.fullLengthMoveCount = moveCount;
+    nativeAPI.world.restMoveCount = 1;
+  
+    const sprite = nativeAPI.makeNewDanceSprite("CAT", null, {x: 200, y: 200});  
+
+    testCode({ nativeAPI, sprite });
+
+    nativeAPI.reset();
+  }
+
+  // Verify invalid string parameter behavior:
+  await subTest({ testCode: ({ nativeAPI, sprite }) => {
+    let error = null;
+    try {
+      // Passing 'some_string' should fail
+      nativeAPI.changeMoveLR(sprite, 'some_string', 1);
+    } catch (e) {
+      error = e;
+    }
+    t.notEqual(error, null, "invalid string parameter");
+  }});
+
+  // Verify invalid move index behavior for changeMoveLR():
+  await subTest({ testCode: ({ nativeAPI, sprite }) => {
+    let error = null;
+    try {
+      // Passing 3 should fail (index is too large)
+      nativeAPI.changeMoveLR(sprite, 3, 1);
+    } catch (e) {
+      error = e;
+    }
+    t.notEqual(error, null, "invalid move index for changeMoveLR");
+  }});
+
+  // Verify invalid rand move because we don't have any different, non-resting moves:
+  await subTest({ moveCount: 2, testCode: ({ nativeAPI, sprite }) => {
+    let error = null;
+    try {
+      // Passing 'rand' should fail
+      nativeAPI.changeMoveLR(sprite, 'rand', 1);
+    } catch (e) {
+      error = e;
+    }
+    t.notEqual(error, null, "invalid rand move");
+  }});
+
+  // Verify invalid move index behavior for doMoveLR():
+  await subTest({ testCode: ({ nativeAPI, sprite }) => {
+    let error = null;
+    try {
+      // Passing 3 should fail (index is too large)
+      nativeAPI.doMoveLR(sprite, 3, 1);
+    } catch (e) {
+      error = e;
+    }
+    t.notEqual(error, null, "invalid move index for doMoveLR");
+  }});
+
+  t.end();
+});
+
 test('getCurrentDance returns current move value for initialized sprite and undefined for uninitialized sprite', async t => {
   const nativeAPI = await helpers.createDanceAPI();
   nativeAPI.play({
