a1784eb5753c4b0a595dd62524f4549ee7a88858
Brent Van Minnen <bjvanminnen@gmail.com>
2018-11-06 09:30:56 -0800

    fix test

--- test/unit/modifySongDataTest.js
+++ test/unit/modifySongDataTest.js
@@ -7,16 +7,19 @@ test('can successfully modify song data', t => {
   // make sure we've returned a new object
   t.ok(modified !== songData);
 
+  // pick an arbitrary frame (just needs to be one with non-zero energy)
+  const frame = 1000;
+
   // this is a pretty weak test. just make sure that we're giving different energy values
-  t.notEqual(songData.analysis[100].energy[0], modified.analysis[100].energy[0]);
+  t.notEqual(songData.analysis[frame].energy[0], modified.analysis[frame].energy[0]);
 
   // again pretty weak, but make sure that modifying our params gives us different
   // energy values
   const modifiedSmoothing = modifySongData(songData, 1.5, 0.5);
   const modifiedDeviations = modifySongData(songData, 2, 0.7);
 
-  t.notEqual(modified.analysis[100].energy[0], modifiedSmoothing.analysis[100].energy[0]);
-  t.notEqual(modified.analysis[100].energy[0], modifiedDeviations.analysis[100].energy[0]);
+  t.notEqual(modified.analysis[frame].energy[0], modifiedSmoothing.analysis[frame].energy[0]);
+  t.notEqual(modified.analysis[frame].energy[0], modifiedDeviations.analysis[frame].energy[0]);
 
   t.end();
 });
