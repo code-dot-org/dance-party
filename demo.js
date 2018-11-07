15220f01158fd2cbfce5680224e7151cabe4fda5
Brent Van Minnen <bjvanminnen@gmail.com>
2018-11-07 14:22:24 -0800

    share interpreter injection code

--- demo.js
+++ demo.js
@@ -1,47 +1,7 @@
 import DanceParty from './src/p5.dance';
 import jazzy_beats from './metadata/jazzy_beats';
-const DanceAPI = require('./src/api');
-const interpreted = require('raw-loader!./src/p5.dance.interpreted.js');
-
-/**
- * Inject p5.dance.interpreted code directly (this is only acceptable because we
- * don't have untrusted user input in our dev env). Also attach all functionNames
- * to the global namespace so that they can be called from the console, making
- * for easier testing
- */
-function injectInterpreted(api) {
-  // This list is not exhaustive, and more can/should be added as needed
-  const functionNames = [
-    'atTimestamp',
-    'everySeconds',
-    'everySecondsRange',
-    'everyVerseChorus',
-    'runUserSetup',
-    'runUserEvents',
-    'getCueList',
-  ];
-
-  const globals = new DanceAPI(api);
-  const code = interpreted + `return {${functionNames.join(',')}};`;
-
-  const params = [];
-  const args = [];
-  for (let k of Object.keys(globals)) {
-    params.push(k);
-    args.push(globals[k]);
-  }
-  params.push(code);
-  const ctor = function () {
-    return Function.apply(this, params);
-  };
-  ctor.prototype = Function.prototype;
-  const result = new ctor().apply(null, args);
-
-  // inject onto window
-  functionNames.forEach(name => window[name] = result[name]);
-
-  return result;
-}
+import interpreted from 'raw-loader!./src/p5.dance.interpreted.js';
+import injectInterpreted from './test/helpers/injectInterpreted';
 
 const nativeAPI = window.nativeAPI = new DanceParty({
   onPuzzleComplete: () => {},
@@ -49,16 +9,28 @@ const nativeAPI = window.nativeAPI = new DanceParty({
     callback && callback();
   }, 0),
   onInit: () => {
-    const { runUserSetup, runUserEvents, getCueList } = injectInterpreted(nativeAPI);;
-
-    // setup event tracking
-    nativeAPI.addCues(getCueList());
-    nativeAPI.onHandleEvents = currentFrameEvents => runUserEvents(currentFrameEvents);
-    runUserSetup();
+    const {
+      runUserSetup,
+      runUserEvents,
+      getCueList,
+      atTimestamp
+    } = injectInterpreted(nativeAPI, interpreted);
 
     // Sample user code:
     nativeAPI.setBackgroundEffect('disco');
     nativeAPI.makeNewDanceSprite("CAT", null, {x: 200, y: 200});
+    atTimestamp(2, "measures", function () {
+      nativeAPI.setBackgroundEffect('diamonds');
+    });
+
+
+    // setup event tracking
+    // Worth noting that any calls that setup event tracking (such as atTimestamp)
+    // must happen before this point
+    nativeAPI.addCues(getCueList());
+    nativeAPI.onHandleEvents = currentFrameEvents => runUserEvents(currentFrameEvents);
+    runUserSetup();
+
 
     nativeAPI.play(jazzy_beats);
   },
