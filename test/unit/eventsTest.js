5e3003969bdb8099454d7ad49760d0e945bea60a
Josh Lory <josh.lory@code.org>
2018-11-08 15:58:11 -0800

    More complex ordering test, with conflicting "every" and "after" events

--- test/unit/eventsTest.js
+++ test/unit/eventsTest.js
@@ -24,3 +24,36 @@ test('atTimestamp adds a cue to the cue list', async t => {
   t.end();
   nativeAPI.reset();
 });
+
+test('conflicting everySeconds and atTimestamp cues', async t => {
+  const {nativeAPI, interpretedAPI} = await runUserCode(`
+    everySeconds(2, "seconds", () => setBackground("green"));
+    everySeconds(4, "seconds", () => setBackground("blue"));
+    atTimestamp(6, "seconds", () => setBackground("red"));
+  `);
+
+  function goToSecondsAndVerify(n, expectedColor) {
+    interpretedAPI.runUserEvents({
+      any: true,
+      'cue-seconds': {
+        [n]: true,
+      }
+    });
+    t.deepEqual(nativeAPI.world.background_color, expectedColor);
+  }
+
+  goToSecondsAndVerify(1, undefined);
+  goToSecondsAndVerify(2, 'green');
+  goToSecondsAndVerify(3, 'green');
+  goToSecondsAndVerify(4, 'blue');
+  goToSecondsAndVerify(5, 'blue');
+  goToSecondsAndVerify(6, 'red');
+  goToSecondsAndVerify(7, 'red');
+  goToSecondsAndVerify(8, 'blue');
+  goToSecondsAndVerify(9, 'blue');
+  goToSecondsAndVerify(10, 'green');
+  goToSecondsAndVerify(11, 'green');
+
+  t.end();
+  nativeAPI.reset();
+});
