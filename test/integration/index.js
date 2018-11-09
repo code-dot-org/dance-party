eb00a9207c8d2334b718886da3922352f3478d32
Chris Pirich <chris@code.org>
2018-11-09 08:05:10 -0800

    fix lint issue with nounused vars

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 const effectsLevelTest = require('./effectsLevelTest');
 const hourOfCodeTest = require('./hourOfCodeTest');
 const spriteDanceTest = require('./spriteDanceTest');
