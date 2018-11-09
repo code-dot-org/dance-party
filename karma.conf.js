8213cebd87196bd84879ae25e4c6dddf179c700d
Chris Pirich <chris@code.org>
2018-11-09 14:22:32 -0800

    PR feedback: cleanup karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -13,8 +13,6 @@ module.exports = config => {
       node: {
         fs: 'empty',
       },
-      // Uncomment when debugging
-      // devtool: 'inline-source-map',
       module: {
         rules: [{
           enforce: 'post',
@@ -30,14 +28,7 @@ module.exports = config => {
     reporters: ['tap-pretty', 'coverage'],
 
     // Run headless unless WATCH=1.
-    browsers: process.env['WATCH'] ? ['Chrome'] : ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
-
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: [process.env['WATCH'] ? 'Chrome' : 'ChromeHeadless'],
 
     // Run and exit unless WATCH=1.
     singleRun: !process.env['WATCH'],
