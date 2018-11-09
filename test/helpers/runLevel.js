3d83cd45887635f4f9be9a7e34feaf5f40dda971
Chris Pirich <chris@code.org>
2018-11-09 14:47:55 -0800

    create createDanceAPIWithDefaultMoves for a more normal init, use for integration tests

--- test/helpers/runLevel.js
+++ test/helpers/runLevel.js
@@ -1,11 +1,11 @@
-const {createDanceAPI} = require('./createDanceAPI');
+const {createDanceAPIWithDefaultMoves} = require('./createDanceAPI');
 
 const interpreted = require('raw-loader!../../src/p5.dance.interpreted.js');
 const injectInterpreted = require('./injectInterpreted');
 
 module.exports = (userCode, validationCode, onPuzzleComplete, bpm = 1200) => {
   let nativeAPI;
-  createDanceAPI({
+  createDanceAPIWithDefaultMoves({
     onPuzzleComplete: (result, message) => {
       onPuzzleComplete(result, message);
       nativeAPI.reset();
@@ -21,20 +21,6 @@ module.exports = (userCode, validationCode, onPuzzleComplete, bpm = 1200) => {
         runUserEvents,
         getCueList
       } = injectInterpreted(nativeAPI, interpreted, userCode);
-
-      // Mock 4 cat and moose animation poses.
-      const moveCount = 10;
-      for (let i = 0; i < moveCount; i++) {
-        api.setAnimationSpriteSheet("CAT", i, {}, () => {});
-        api.setAnimationSpriteSheet("MOOSE", i, {}, () => {});
-        api.setAnimationSpriteSheet("ROBOT", i, {}, () => {});
-        api.world.MOVE_NAMES.push({
-          name: `move${i}`
-        });
-      }
-
-      api.world.fullLengthMoveCount = moveCount;
-      api.world.restMoveCount = 1;
 
       api.addCues(getCueList());
       api.onHandleEvents = currentFrameEvents => runUserEvents(currentFrameEvents);
