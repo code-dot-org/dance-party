15a9c063c21fc414741ff254a3b20f92081cf4f7
Josh Lory <josh.lory@code.org>
2018-11-15 15:25:37 -0800

    Better packing of hexagons

--- src/Effects.js
+++ src/Effects.js
@@ -437,60 +437,34 @@ module.exports = class Effects {
         this.shapes.fill('white');
         this.shapes.noStroke();
         this.shapes.angleMode(p5.DEGREES);
+
+        this.hex = p5.createGraphics(200, 200);
+        this.hex.pixelDensity(1);
+        this.hex.angleMode(p5.DEGREES);
+
         this.h = Math.sqrt(3) / 2 * 100;
       },
-      diamondA: function () {
-        p5.push();
-        p5.image(this.shapes, -50, 0);
-        p5.rotate(-30);
-        p5.scale(-1, 1);
-        p5.rotate(30);
-        p5.image(this.shapes, -50, 0);
-        p5.pop();
+      blitHex: function () {
+        this.hex.push();
+        this.hex.clear();
+        this.hex.translate(100, 100);
+        this.hex.rotate(30);
+        for (let i = 0; i < 3; i++) {
+          this.hex.image(this.shapes, -50, 0);
+          this.hex.scale(-1, 1);
+          this.hex.rotate(60);
+          this.hex.image(this.shapes, -50, 0);
+          this.hex.rotate(60);
+          this.hex.scale(-1, 1);
+        }
+        this.hex.pop();
       },
-      diamondB: function () {
+      row: function (n) {
         p5.push();
-        p5.translate(150, this.h);
-        p5.rotate(120);
-        p5.image(this.shapes, -50, 0);
-        p5.rotate(30);
-        p5.scale(-1, 1);
-        p5.rotate(-30);
-        p5.image(this.shapes, -50, 0);
-        p5.pop();
-      },
-      diamondC: function () {
-        p5.push();
-        p5.translate(150, this.h);
-        p5.rotate(-120);
-        p5.image(this.shapes, -50, 0);
-        p5.translate(100, 0);
-        p5.rotate(120);
-        p5.translate(200, 0);
-        p5.rotate(30);
-        p5.scale(-1, 1);
-        p5.rotate(-30);
-        p5.image(this.shapes, -50, 0);
-        p5.pop();
-      },
-      tessellate: function () {
-        p5.push();
-        this.diamondA();
-        this.diamondB();
-        this.diamondC();
-        p5.translate(300, 0);
-        this.diamondA();
-        this.diamondB();
-        p5.pop();
-
-        p5.push();
-        p5.translate(-150, this.h);
-        this.diamondB();
-        this.diamondC();
-        p5.translate(300, 0);
-        this.diamondA();
-        this.diamondB();
-        this.diamondC();
+        for (let i = 0; i < n; i++) {
+          p5.image(this.hex, 0, 0);
+          p5.translate(this.h * 2, 0);
+        }
         p5.pop();
       },
       draw: function () {
@@ -507,6 +481,7 @@ module.exports = class Effects {
         ctx.lineTo(100, this.h);
         ctx.lineTo(0, this.h);
         ctx.clip();
+        this.shapes.clear();
         this.shapes.rotate(p5.frameCount);
         this.shapes.rect(20, 20, 50, 50);
         this.shapes.fill('#082036');
@@ -520,28 +495,33 @@ module.exports = class Effects {
         this.shapes.fill('#F0DFA2');
         this.shapes.rect(-45, 0, 30, 30);
         this.shapes.rotate(17);
-        this.shapes.fill('#271A3A');
+        this.shapes.fill('#717171');
         this.shapes.rect(30, 40, 10, 40);
         this.shapes.rotate(37);
-        this.shapes.fill('white');
+        this.shapes.fill('#5b2c6e');
         this.shapes.rect(30, 40, 20, 40);
         ctx.restore();
 
         p5.push();
-        // p5.translate(200, 200);
-        // p5.rotate(-p5.frameCount);
-        // p5.translate(-200, -200);
-        p5.translate(0, -this.h / 3);
-        this.tessellate();
-        p5.translate(0, this.h * 2);
-        this.tessellate();
-        p5.translate(0, this.h * 2);
-        this.diamondA();
-        this.diamondB();
-        this.diamondC();
-        p5.translate(300, 0);
-        this.diamondA();
-        this.diamondB();
+        this.blitHex();
+        p5.imageMode(p5.CENTER);
+
+        p5.translate(200, 200);
+        p5.rotate(p5.frameCount);
+        p5.scale(0.8);
+
+        p5.translate(this.h * -2, -300);
+        this.row(3);
+        p5.translate(-this.h, 150);
+        this.row(4);
+        p5.translate(-this.h, 150);
+        this.row(5);
+        p5.translate(this.h, 150);
+        this.row(4);
+        p5.translate(this.h, 150);
+        this.row(3);
+
+
         p5.pop();
       }
     };
