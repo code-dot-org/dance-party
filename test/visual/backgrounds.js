2939fbd3c2821eae17af09cbeb4d5ec11f192487
Erin Peach <erin@code.org>
2018-11-16 13:00:33 -0800

    Remove generating diff png

--- test/visual/backgrounds.js
+++ test/visual/backgrounds.js
@@ -40,8 +40,6 @@ async function testBackground(t, effect) {
     var diff = new PNG({width: img1.width, height: img1.height});
 
     let pixelDiff = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1});
-
-    diff.pack().pipe(fs.createWriteStream('diff.png'));
     t.equal(pixelDiff, 0, effect);
   }
 }
