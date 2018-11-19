7c50b8b779fe074ef50720e7bfbc14b36a0f1bdd
anthony suarez <anthony@code.org>
2018-11-19 01:11:05 -0800

    v2 add floating stars background
    add floating stars background

--- src/Effects.js
+++ src/Effects.js
@@ -600,6 +600,36 @@ module.exports = class Effects {
         });
       }
     };
+
+    this.stars = {
+      star: [],
+      draw: function () {
+        p5.background('black');
+        let star = {
+          x: p5.random(0, 400),
+          y: p5.random(0, 400),
+          size: p5.random(15, 30),
+        };
+        this.star.push(star);
+        p5.noStroke();
+        p5.fill(255, 255, 0);
+        this.star.forEach(function (star){
+          p5.push();
+          p5.translate(star.x, star.y);
+          for (let i = 0; i < 3; i++) {
+            p5.rotate(360 / 5);
+            p5.ellipse(0, 0, 1, star.size);
+          }
+          let fadeSpeed = p5.map(star.size, 15, 30, 1, 2);
+          star.size = star.size - fadeSpeed;
+          star.y = star.y - 2;
+          p5.pop();
+        });
+        this.star = this.star.filter(function (star) {
+          return star.size > 0.1;
+        });
+      }
+    };
   }
 };
 
