ed8ec154db5352feabe9ff9e6476dd3aef2024f2
Erin Peach <erin@code.org>
2018-11-27 17:07:57 -0800

    Update visual background tests to handle palettes

--- test/visual/helpers/createBackgroundScreenshot.js
+++ test/visual/helpers/createBackgroundScreenshot.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 /*
   Set the background to given effect and save a screenshot to pathname
  */
-async function createBackgroundScreenshot(effectName, pathname){
+async function createBackgroundScreenshot(effectName, pathname, palette){
   let nativeAPI = await helpers.createDanceAPI();
   nativeAPI.p5_.randomSeed(0);
   nativeAPI.p5_.noiseSeed(0);
-  nativeAPI.setBackgroundEffect(effectName);
+  nativeAPI.setBackgroundEffect(effectName, palette);
   for (let i = 0; i < 100; i++) {
     nativeAPI.p5_.background('#fff');
     nativeAPI.getBackgroundEffect().draw({bpm: 0, centroid: 7000, artist: 'artist', title: 'title', isPeak: i % 6 === 0});
@@ -21,7 +21,9 @@ async function createBackgroundScreenshot(effectName, pathname){
     fs.mkdirSync(pathname);
   }
 
-  fs.writeFileSync(`${pathname}/${effectName}.png`, buffer);
+  let name = effectName + (palette ? ("_" + palette) : "");
+
+  fs.writeFileSync(`${pathname}/${name}.png`, buffer);
   nativeAPI.reset();
 }
 
