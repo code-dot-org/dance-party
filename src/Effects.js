8e362fb6e9ab5333868141b60818e49350c9c732
Josh Lory <josh.lory@code.org>
2018-12-04 14:16:27 -0800

    Add background effect with randomly moving quads

--- src/Effects.js
+++ src/Effects.js
@@ -668,6 +668,50 @@ module.exports = class Effects {
       }
     };
 
+    this.quads = {
+      shapes: [],
+      init: function () {
+        if (this.buffer) {
+          return;
+        }
+        this.buffer = p5.createGraphics(400, 400);
+        this.buffer.noFill();
+        this.buffer.stroke('#0f0');
+        this.buffer.strokeWeight(0.5);
+        this.buffer.background(0);
+
+        for (let i = 0; i < 2; i++) {
+          const shape = [];
+          for (let j = 0; j < 4; j++) {
+            const vertex = p5.createSprite();
+            vertex.draw = () => {};
+            vertex.position = p5.createVector(p5.random(0, 400), p5.random(0, 400));
+            vertex.velocity = p5.createVector(0, 1).rotate(p5.random(0,360));
+            shape.push(vertex);
+          }
+          this.shapes.push(shape);
+        }
+        this.edges = p5.createEdgeSprites();
+      },
+      draw: function () {
+        this.buffer.background(0, 10);
+
+        for (const shape of this.shapes) {
+          this.buffer.quad.apply(this.buffer, shape.reduce((acc, current) => {
+            current.bounceOff(this.edges);
+            acc.push(current.position.x, current.position.y);
+            return acc;
+          }, []));
+        }
+
+        // Copy the off-screen buffer to the canvas.
+        p5.push();
+        p5.scale(1 / p5.pixelDensity());
+        p5.drawingContext.drawImage(this.buffer.elt, 0, 0);
+        p5.pop();
+      }
+    };
+
     this.color_lights = {
       lights: [],
       newLight: function (x, arc, offset) {
