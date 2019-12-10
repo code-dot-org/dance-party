b1e1a24617ea3a023e9e005095c61bf955be1a46
Anjali <anjali@code.org>
2019-12-09 16:38:17 -0800

    separate tests

--- test/unit/replayTest.js
+++ test/unit/replayTest.js
@@ -14,12 +14,13 @@ class mockP5 {
     });
   }
 
+  // Some effects (ex. quads) create sprites which are not dancers
   createEffectsSprite() {
     this.allSprites.push({});
   }
 }
 
-test('replay', t => {
+test('Dance sprites are added to replay log', t => {
   const p5Inst = new mockP5();
 
   replayLog.logFrame({p5: p5Inst});
@@ -31,10 +32,27 @@ test('replay', t => {
   t.equal(replayLog.getLog().length, 2);
   t.equal(replayLog.getLog()[1].sprites.length, 1);
 
-  p5Inst.createEffectsSprite();
+  p5Inst.createDanceSprite();
   replayLog.logFrame({p5: p5Inst});
   t.equal(replayLog.getLog().length, 3);
-  t.equal(replayLog.getLog()[1].sprites.length, 1);
+  t.equal(replayLog.getLog()[2].sprites.length, 2);
 
+  replayLog.reset();
   t.end();
 });
+
+test('Effects sprites are not added to replay log', t => {
+  const p5Inst = new mockP5();
+
+  replayLog.logFrame({p5: p5Inst});
+  t.equal(replayLog.getLog().length, 1);
+  t.equal(replayLog.getLog()[0].sprites.length, 0);
+  
+  p5Inst.createEffectsSprite();
+  replayLog.logFrame({p5: p5Inst});
+  t.equal(replayLog.getLog().length, 2);
+  t.equal(replayLog.getLog()[1].sprites.length, 0);
+  
+  replayLog.reset();
+  t.end();
+})
