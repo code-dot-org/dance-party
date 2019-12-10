19e65d5f5f45f4a1b24f2767f894c29ec5d21cb3
Anjali <anjali@code.org>
2019-12-09 16:26:49 -0800

    Update test

--- test/unit/replayTest.js
+++ test/unit/replayTest.js
@@ -6,11 +6,16 @@ class mockP5 {
     this.allSprites = [];
   }
 
-  createSprite() {
+  createDanceSprite() {
     this.allSprites.push({
       getAnimationLabel: () => {},
       mirrorX: () => {},
+      isDancer: true
     });
+  }
+
+  createEffectsSprite() {
+    this.allSprites.push({});
   }
 }
 
@@ -21,9 +26,14 @@ test('replay', t => {
   t.equal(replayLog.getLog().length, 1);
   t.equal(replayLog.getLog()[0].sprites.length, 0);
 
-  p5Inst.createSprite();
+  p5Inst.createDanceSprite();
   replayLog.logFrame({p5: p5Inst});
   t.equal(replayLog.getLog().length, 2);
+  t.equal(replayLog.getLog()[1].sprites.length, 1);
+
+  p5Inst.createEffectsSprite();
+  replayLog.logFrame({p5: p5Inst});
+  t.equal(replayLog.getLog().length, 3);
   t.equal(replayLog.getLog()[1].sprites.length, 1);
 
   t.end();
