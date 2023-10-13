724873191bd95b41b7327453e048c396425a979d
Alice Fisher <alice.fisher@code.org>
2023-10-13 01:13:14 +0000

    add poetry lab palettes

--- src/constants.js
+++ src/constants.js
@@ -101,6 +101,19 @@ module.exports = {
     tropical: ['#eb6493', '#72d7fb', '#7efaaa', '#fffe5c', '#ee8633'],
     neon: ['#e035a1', '#a12dd3', '#58b0ed', '#75e847', '#fdf457'],
     rave: ['#000000', '#5b6770', '#c6cacd', '#e7e8ea', '#ffffff'],
+    // Color palettes from poetry lab.
+    grayscale: ['#000000', '#333333', '#666666', '#999999', '#CCCCCC', '#EEEEEE', '#FFFFFF'],
+    sky: ['#3878A4', '#82A9B1', '#ECCEC4', '#F8B8A8', '#E4929C', '#7D7095'],
+    ocean: ['#7FD0F5', '#3FABE3', '#2C7DBB', '#1D57A0', '#144188', '#061F4B'],
+    sunrise: ['#F5DC72', '#F4B94F', '#F48363', '#F15C4C', '#372031'],
+    sunset: ['#530075', '#921499', '#E559BB', '#F7B9DD', '#307087', '#123F50'],
+    spring: ['#303F06', '#385202', '#547607', '#85AF4C', '#C1E876', '#D7FF6B'],
+    summer: ['#FAD0AE', '#F69F88', '#EE6E51', '#BC4946', '#425D19', '#202E14'],
+    autumn: ['#484F0C', '#AEA82E', '#EEBB10', '#D46324', '#731B31', '#4A173C'],
+    winter: ['#EAECE8', '#E3DDDF', '#D3CEDC', '#A2B6BF', '#626C7D', '#A4C0D0'],
+    twinkling: ['#FFC702', '#FC9103', '#F17302', '#B83604', '#7E1301'],
+    rainbow: ['#A800FF', '#0079FF', '#00F11D', '#FF7F00', '#FF0900'],
+    roses: ['#4C0606', '#86003C', '#E41F7B', '#FF8BA0 ', '#FFB6B3'],
   },
   HIGHER_POWER_COLORS: {
     default: ['#161317', '#2143C5'], // light: black to blue
