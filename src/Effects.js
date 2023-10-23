3ae5eea0624e88272c9fce48559a26b768871d56
Ben Brooks <ben@code.org>
2023-10-23 15:46:37 -0700

    Working lasers background

--- src/Effects.js
+++ src/Effects.js
@@ -1069,23 +1069,24 @@ module.exports = class Effects {
       draw: function () {
         p5.background('black');
         if (this.laser.length < 32) {
-          let laser = {
-            w: 200,
-            x: 200,
-            y: 1750,
-            z: 400,
-            color: lerpColorFromPalette(p5.frameCount / 16),
-          };
-          this.laser.push(laser);
+          const numLasersToDraw = this.getPreviewCustomizations().numLasersToDraw;
+          for (let i = 0; i < numLasersToDraw; i++) {
+            let laser = {
+              y: this.getPreviewCustomizations().getY(i),
+              z: 400,
+              color: this.getPreviewCustomizations().getColor(i)
+            };
+            this.laser.push(laser);
+          }
         }
         p5.push();
         p5.translate(200, 180);
-        for (const laser of this.laser) {
-          let y = 200 * p5.sin(p5.frameCount);
-          if (y < 0) {
-            y *= -1;
+        for (const [index, laser] of this.laser.entries()) {
+          let x = this.getPreviewCustomizations().getX(index);
+          if (x < 0) {
+            x *= -1;
           }
-          const angle = p5.atan2(laser.y, y);
+          const angle = p5.atan2(laser.y, x);
           p5.stroke(laser.color);
           p5.line(0, 0, p5.sin(angle) * 300, p5.cos(angle) * 300);
           laser.y = laser.y - 100;
@@ -1094,6 +1095,28 @@ module.exports = class Effects {
           }
         }
         p5.pop();
+      },
+      reset: function () {
+        this.laser = [];
+      },
+      // Note to future readers: at the time of writing,
+      // this implementation felt particularly hacky relative to other preview customizations.
+      // It's more challenging than many others, because the effect is "less random" than many other previews.
+      // Definitely open to alternative implementations as time allows!
+      getPreviewCustomizations: function () {
+        return getInPreviewMode() ?
+          {
+            numLasersToDraw: 32,
+            getY: iterationNum => 1750 - (iterationNum * 100),
+            getColor: iterationNum => lerpColorFromPalette(iterationNum / 16),
+            getX: iterationNum => 200 * p5.sin(iterationNum),
+          } :
+          {
+            numLasersToDraw: 1,
+            getY: () => 1750,
+            getColor: () => lerpColorFromPalette(p5.frameCount / 16),
+            getX: () => 200 * p5.sin(p5.frameCount),
+          };
       },
     };
 
