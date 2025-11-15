f66063a923055f2430f9f2fe9ada4d419fff9e04
breville <brendan@code.org>
2025-11-15 04:07:55 +0000

    Update

--- src/p5.dance.js
+++ src/p5.dance.js
@@ -693,7 +693,6 @@ module.exports = class DanceParty {
         sprite.mirrorX(
           this.generatedDancer.shouldMirror(this.getCurrentMeasure()) ? -1 : 1
         );
-
         this.p5_.image(
           this.generatedDancer.graphics,
           sprite.x - location.x,
